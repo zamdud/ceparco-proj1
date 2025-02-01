b4ea63f6e2f722953d21b19623b7e3aaaf294b03
zamdud <geohmen@gmail.com>
2025-02-02 00:12:33 +0800

    Finished CKernel

--- C/CKernel.c
+++ C/CKernel.c
@@ -3,9 +3,35 @@
 // #include <windows.h>
 #include <time.h>
 
-int main()
+void CKernel(size_t ARRAY_SIZE, int* x, int* y)
 {
+    // const size_t ARRAY_BYTES = ARRAY_SIZE * sizeof(INT32); //For windows
+    const size_t ARRAY_BYTES = ARRAY_SIZE * 4; //MacOS
 
+    //Declare Dynamic Array
+    // int *x, *y;
 
-    return 0;
+    // x = (int*)malloc(ARRAY_BYTES);
+    // y = (int*)malloc(ARRAY_BYTES);
+    
+    
+    //Initialize Array
+
+    for(int i = 0; i<=ARRAY_BYTES; i++)
+    {
+        x[i] = i+1;
+        y[i] = 0;
+    }
+
+    //Addition
+    for(int i=3;i<ARRAY_SIZE-3;i++)
+    {
+        y[i] = x[i-3] + x[i-2] + x[i-1] + x[i] + x[i+1] + x[i+2] + x[i+3];
+    }
+
+    // for(int i=3;i<ARRAY_SIZE-3;i++)
+    // {
+    //     printf("y[%d] = %d\n",i,y[i]);
+    // }
+
 }
