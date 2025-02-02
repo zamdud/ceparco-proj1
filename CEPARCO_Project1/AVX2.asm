8b6ff8b13a400aa908e485859cb138312deb00d9
Ashantie1223 <ashantie_demanalata@dlsu.edu.ph>
2025-02-02 23:02:05 +0800

    OPTIMIZED AVX2

--- CEPARCO_Project1/AVX2.asm
+++ CEPARCO_Project1/AVX2.asm
@@ -1,6 +1,5 @@
 section .data
-mask1 dd -1,-1,-1,-1,-1,-1,-1,0
-mask2 dd 0,-1,-1,-1,-1,-1,-1,-1
+mask1 dd -1,-1,-1,-1,-1,-1,-1,0 
 
 section .text
 bits 64
@@ -13,72 +12,55 @@ AVX2Kernel:
     ;rdx = address of X
     ;r8 = address of Y
     sub rcx, 6
-    add r8, 3*4
-
+    mov rsi, 3
+    
     mov rax, rcx ; Flag for handling edge case
-    shr rax, 2
-    imul rax, -4
-    add rax, rcx
-    sub rcx, rax
     shr rcx, 2
-
+    shl rcx, 2
+    sub rax, rcx 
+    
     cmp rcx, 0
     jz SKIP1
+    
+L1: vmovdqu ymm0, [rdx+rsi*4-12]
+    vmovdqu ymm1, [rdx+rsi*4-4] 
+    vperm2i128 ymm2, ymm0, ymm0, 1
+    vperm2i128 ymm3, ymm1, ymm1, 1
+    vpaddd ymm4, ymm2, ymm0
+    vpaddd ymm5, ymm3, ymm1
+    vphaddd ymm4, ymm4, ymm4
+    vphaddd ymm5, ymm5, ymm5
+    vphaddd ymm4, ymm4, ymm4
+    vphaddd ymm5, ymm5, ymm5 
+    vpsubd ymm0, ymm4, ymm0
+    vpsubd ymm1, ymm5, ymm1
+    vpsubd ymm2, ymm4, ymm2
+    vpsubd ymm3, ymm5, ymm3
+    vshufps xmm0, xmm2, 0b11110000
+    vpshufd xmm0, xmm0, 0b00110011
+    vshufps xmm1, xmm3, 0b11110000
+    vpshufd xmm1, xmm1, 0b00110011
+    vmovq [r8+rsi*4], xmm0
+    vmovq [r8+rsi*4+8], xmm1
+    add rsi, 4
+    cmp rsi, rcx
+    jl L1
+SKIP1:
 
-L1:     vmovdqu ymm0, [rdx]
-        vmovdqu ymm2, [rdx+8]
-        
-        vpand ymm1, ymm0, [mask2]
-        vpand ymm0, ymm0, [mask1]
-        vpand ymm3, ymm2, [mask2]
-        vpand ymm2, ymm2, [mask1]
-
-        vphaddd ymm0, ymm0, ymm0
-        vphaddd ymm1, ymm1, ymm1
-        vphaddd ymm2, ymm2, ymm2
-        vphaddd ymm3, ymm3, ymm3
-        
-        vphaddd ymm0, ymm0, ymm0
-        vphaddd ymm1, ymm1, ymm1
-        vphaddd ymm2, ymm2, ymm2
-        vphaddd ymm3, ymm3, ymm3
-        
-        vpermq ymm4, ymm0, 0b00001110
-        vpermq ymm5, ymm1, 0b00001110
-        vpermq ymm6, ymm2, 0b00001110
-        vpermq ymm7, ymm3, 0b00001110
-        
-        vpaddd ymm0, ymm0, ymm4
-        vpaddd ymm1, ymm1, ymm5
-        vpaddd ymm2, ymm2, ymm6
-        vpaddd ymm3, ymm3, ymm7
-        
-        vmovd [r8], xmm0
-        vmovd [r8+4], xmm1
-        vmovd [r8+8], xmm2
-        vmovd [r8+12], xmm3
-
-        add rdx, 4*4
-        add r8, 4*4
-        dec rcx
-        jnz L1
-SKIP1:    
-    mov rcx, rax
-    cmp rcx, 0
+    cmp rax, 0
     jz SKIP2
-
-L2: vmovdqu ymm0, [rdx]
+    
+L2: vmovdqu ymm0, [rdx+rsi*4-12]
     vpand ymm0, ymm0, [mask1]
     vphaddd ymm0, ymm0, ymm0
     vphaddd ymm0, ymm0, ymm0
     vpermq ymm4, ymm0, 0b00001110
     vpaddd ymm0, ymm0, ymm4
-    vmovd [r8], xmm0
-    add rdx, 4
-    add r8, 4
-    dec rcx
-    jnz L2
-
-SKIP2:
-    ret
+    vmovd [r8+rsi*4], xmm0
+    inc rsi
+    cmp rsi, rax
+    jl L2
     
+SKIP2:
+
+    ret
