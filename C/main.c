bbac21306368faf335d92fba120708b019455d55
zamdud <geohmen@gmail.com>
2025-02-02 10:13:22 +0800

    New Branch

--- C/main.c
+++ C/main.c
@@ -1,22 +1,23 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include "windows.h"
+// #include "windows.h"
+#include "CKernel.c"
 
 
-void CKernel(size_t n, INT32* X, INT32* Y);
-extern void x86Kernel(size_t n, INT32* X, INT32* Y);
-extern void AVXKernel(size_t n, INT32* X, INT32* Y);
-extern void AVX2Kernel(size_t n, INT32* X, INT32* Y);
+void CKernel(size_t n, int* X, int* Y);
+// extern void x86Kernel(size_t n, int X, int Y);
+// extern void AVXKernel(size_t n, INT32* X, INT32* Y);
+extern void AVX2Kernel(size_t n, int* X, int* Y);
 
 int main() {
-	const size_t n = 1<<20;
-	const size_t ARRAY_BYTES = n * sizeof(INT32);
+	const size_t n = 1<<10;
+	const size_t ARRAY_BYTES = n * 4;
 	int i;
 	
 	// DECLARE VECTORS
-	INT32* X, * Y;
-	X = (INT32*)malloc(ARRAY_BYTES);
-	Y = (INT32*)malloc(ARRAY_BYTES);
+	int *X, *Y;
+	X = (int*)malloc(ARRAY_BYTES);
+	Y = (int*)malloc(ARRAY_BYTES);
 
 	// INITIALIZE ARRAYS
 	for (i = 0; i < n; i++) {
@@ -28,7 +29,7 @@ int main() {
 	
 
 	// C Kernel Implementation
-	//CKernel(n, X, Y);
+	// CKernel(n, X, Y);
 	
 	// x86 Kernel Implementation
 	//x86Kernel(n, X, Y);
@@ -37,11 +38,11 @@ int main() {
 	//AVXKernel(n, X, Y);
 
 	// AVX2 Kernel Implementation
-	//AVX2Kernel(n, X, Y);
+	AVX2Kernel(n, X, Y);
 
 	// ERROR CHECKING
 
-	for (i = 3; i < n-3; i++) {
+	for (i = n; i < n-3; i++) {
 		printf("Y[%d] = %d\n", i, Y[i]);
 	}
 
